3075aa902fd7799d5302e99ebae765ac9bcb8f1d
Marcus Elg <melg@kth.se>
2021-11-21 11:13:06 +0100

    Something?

--- go.pl
+++ go.pl
@@ -11,11 +11,36 @@ alive(Row, Column, BoardFileName):-
     read(Board),            % Reads the first Prolog-term from the file
     seen,                   % Closes the io-stream
     nth1_2d(Row, Column, Board, Stone),
-    Stone = e -> false;
-    rec(Row, Column, Board, OriginalStone, []).
+    not(Stone = e),
+    rec(Row, Column, Board, Stone, []).
+
+% Outside board
+rec(0, _, _, _, _):-
+    false, !.
+rec(10, _, _, _, _):-
+    false, !.
+rec(_, 0, _, _, _):-
+    false, !.
+rec(_, 10, _, _, _):-
+    false, !.
+
+% Found empty
+rec(Row, Column, Board, OriginalStone, Visited):-
+    nth1_2d(Row, Column, Board, Stone),
+    Stone = e, !.
 
 % Main recursion
 rec(Row, Column, Board, OriginalStone, Visited):-
-    nth1_2d(Row, Column, Board, Stone),
-    Stone = e -> false;
-    write(t).
+    not(member([Row, Column], Visited)),
+    nth1_2d(Row, Column, Board, Stone), % Get stone at position
+    write(Row),
+    write(Column),
+    (Stone = OriginalStone), 
+    NewRow is Row - 1,
+    NewRow2 is Row + 1,
+    NewColumn is Column - 1,
+    NewColumn2 is Column + 1,
+    rec(NewRow, Column, Board, OriginalStone, [[Row, Column]|Visited]);    
+    rec(NewRow2, Column, Board, OriginalStone, [[Row, Column]|Visited]);
+    rec(Row, NewColumn, Board, OriginalStone, [[Row, Column]|Visited]);
+    rec(Row, NewColumn2, Board, OriginalStone, [[Row, Column]|Visited]).
