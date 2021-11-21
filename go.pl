86bab4042932f93c8930c596d8d502dbfc62ec8e
Marcus Elg <melg@kth.se>
2021-11-21 10:01:39 +0100

    Why is this not working!?

--- go.pl
+++ go.pl
@@ -10,32 +10,12 @@ alive(Row, Column, BoardFileName):-
     see(BoardFileName),     % Loads the input-file
     read(Board),            % Reads the first Prolog-term from the file
     seen,                   % Closes the io-stream
-    nth1_2d(Row, Column, Board, OriginalStone),
-    write(OriginalStone),
-    OriginalStone = e -> false ;
-    write(OriginalStone),
+    nth1_2d(Row, Column, Board, Stone),
+    Stone = e -> false;
     rec(Row, Column, Board, OriginalStone, []).
 
 % Main recursion
 rec(Row, Column, Board, OriginalStone, Visited):-
-    write(recursion),
-    Row = 0 -> false ;
-    Row = 10 -> false ;
-    Column = 0 -> false ;
-    Column = 10 -> false ;
-    %member([Row, Column], Visited) -> false ;
-    nth1_2d(Row, Column, Board, Stone), % Get stone at position
-    nth1_2d(Row, Column, Board, Stone2),
-    write(Stone),
-    write(Stone2),
-    write(Row),
-    write(Column),
-    Stone =:= e -> true ;
-    Stone \= OriginalStone -> false ; 
-    %\+ member([Row, Column], Visited) -> % Return false if already visited
-    NewRow is Row - 1,
-    rec(NewRow, Column, Board, OriginalStone, [[Row, Column]|Visited]);
-    NewRow2 is Row + 1,
-    rec(NewRow2, Column, Board, OriginalStone, [[Row, Column]|Visited]).
-    %rec(Row, Column - 1, _, NewVisited),
-    %rec(Row, Column + 1, _, NewVisited).
+    nth1_2d(Row, Column, Board, Stone),
+    Stone = e -> false;
+    write(t).
